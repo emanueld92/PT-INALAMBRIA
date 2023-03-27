59f432bad2049cfe7fa1343e987acfc37cbf74da
Emanueld92 <emanuelx92@gmail.com>
2023-03-27 00:29:19 -0400

    feature(Ordering): sort List fichas

--- src/Inalambria.Domino/Inalambria.Domino.ApplicationServices/Ordering/DominoServices.cs
+++ src/Inalambria.Domino/Inalambria.Domino.ApplicationServices/Ordering/DominoServices.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,20 +15,61 @@ namespace Inalambria.Domino.ApplicationServices.Ordering
         List<string> IDominoServices.OrderDomino(List<string> fichas)
         {
 
-            foreach(var f  in fichas)
+            List<string> result = new List<string>();
+
+
+
+
+            
+            string firstDomino = fichas[0];
+            result.Add(firstDomino);
+            fichas.Remove(firstDomino);
+
+            while (fichas.Count > 0)
             {
-                Console.WriteLine(f[0]);
-                Console.WriteLine(f[1]);
+                foreach(var ficha in fichas)
+                {
+                    var Ultima = result.Last();
+                    if (Ultima[3] == ficha[1])
+                    {
+                        result.Add(ficha);
+                        fichas.Remove(ficha);
+                        break;
+
+                    }
+                    else if (Ultima[3] == ficha[3])
+                    {
+                        var invertida = $"[{ficha[3]}|{ficha[1]}]";
+                        result.Add(invertida);
+                        fichas.Remove(ficha);
+                        break;
+                    }
+                    
+                    
+                
+                };
+
+                ////string nextDomino = fichas.FirstOrDefault(s => s.StartsWith($"[{result.Last().Split('|')[1]}|") || s.EndsWith($"|{result.Last().Split('|')[1]}]"));
+                //if (nextDomino != null)
+                //{
+                //    result.Add(nextDomino);
+                //    fichas.Remove(nextDomino);
+                //}
+                //else
+                //{
+                //    break;
+                //}
             }
 
-
-            List<string> result = new List<string>(
-                fichas
-
-
-                ) ;
-            
-
+            if (fichas.Count > 0)
+            {
+                Console.WriteLine("No se pueden ordenar las fichas según la condición dada.");
+                
+            }
+            else
+            {
+                Console.WriteLine($"[{string.Join("],[", result)}]");
+            }
 
 
             return (result);
